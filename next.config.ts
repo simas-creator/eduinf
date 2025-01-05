bcac3964eb2fde13b976b582d3962a114a9c1e5a
Simas G <simas@simson.lt>
2025-01-05 10:10:28 +0200

    testas

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  unstable_allowDynamic: [
-    '/lib/utilities.js', 
-    '**/node_modules/function-bind/**',
-    '@lib/mongodb',
-    'app/api/login/route.ts',
-  ],
+  
 };
 
 export default nextConfig;
